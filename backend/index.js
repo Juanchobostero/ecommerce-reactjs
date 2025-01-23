134694b4514b1a50c031b1d05c3b840ecc33a24b
JUANCHO <juancruzmart93@gmail.com>
2025-01-23 13:10:48 -0300

    arreglo mi rama

--- backend/index.js
+++ backend/index.js
@@ -12,41 +12,52 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import mercadopago from 'mercadopago';
 import cors from 'cors';
-import { rateLimit } from "express-rate-limit";
 
-const limiterGeneral = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 min
-    limit: 10, // Incrementé el límite para evitar bloqueos constantes
-    standardHeaders: true,
-    legacyHeaders: false,
+mercadopago.configure({
+	access_token: "APP_USR-5029250925841563-081611-6d6c33c264b920c6cce47d975f80e384-1180488044",
 });
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
-// const allowedOrigins = [
-//     'http://localhost:3000',
-//     'http://localhost:3420',
-//     'https://ecommerce-reactjs-chi.vercel.app',
-//     'https://ecommerce-reactjs-client-git-juancho-juanchobosteros-projects.vercel.app',
-//     'https://ecommerce-reactjs-client-git-test-juanchobosteros-projects.vercel.app'
-// ];
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3420',
+    'https://ecommerce-reactjs-chi.vercel.app', // PROD
+    'https://ecommerce-reactjs-client-git-test-juanchobosteros-projects.vercel.app' // QA
+];
 
-app.use(cors());
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true, // Si usas cookies o sesiones
+};
 
+app.use(cors(corsOptions));
 
-if (process.env.NODE_ENV === 'development') {
+// Maneja solicitudes preflight
+app.options('*', cors(corsOptions));
+
+if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 } else {
-    app.use(morgan('combined'));
+	app.use(morgan('combine'));
 }
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('API is running...');
+    res.send('API is running ...');
 });
 
 app.use('/api/products', productRoutes);
@@ -55,75 +66,72 @@ app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID);
+app.get('/api/config/paypal', (req, res) => 
+    res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+// PREFERENCIA MERCADO PAGO
+app.post("/create_preference", (req, res) => {
+	const url = process.env.NODE_ENV === 'development' 
+		? 'http://localhost:3000' 
+		: process.env.NODE_BASE_URI_CLIENT_PROD;
+	let cartItemsMercadoPago = req.body.orderDataMercadoPago.map((item) => ({
+		...item,
+		title: item.name,
+		unit_price: Number(item.price),
+		quantity: Number(item.quantity)
+	}));
+	let order = req.body.order;
+	let preference = {
+		items: cartItemsMercadoPago,
+		back_urls: {
+			"success": `${url}/order/${order}`,
+			"failure": `${url}/order/${order}`,
+			"pending": `${url}/order/${order}`
+		},
+		auto_return: "approved",
+		statement_descriptor: "ECOMMERCE JUAN",
+		//notification_url: 'https://1281-201-235-90-251.ngrok-free.app/notify'
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			console.log(response.body);
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
 });
 
-app.post('/create_preference', async (req, res) => {
-    try {
-        const url = process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3420'
-            : process.env.REACT_APP_URI_FRONT_PRODUCTION || 'https://ecommerce-reactjs-client-git-test-juanchobosteros-projects.vercel.app';
-        
-        let cartItemsMercadoPago = req.body.orderDataMercadoPago.map((item) => ({
-            ...item,
-            title: item.name,
-            unit_price: Number(item.price),
-            quantity: Number(item.quantity)
-        }));
-        let order = req.body.order;
-        let preference = {
-            items: cartItemsMercadoPago,
-            back_urls: {
-                "success": `${url}/order/${order}`,
-                "failure": `${url}/order/${order}`,
-                "pending": `${url}/order/${order}`
-            },
-            auto_return: 'approved',
-            statement_descriptor: 'ECOMMERCE JUAN',
-        };
-
-        const response = await mercadopago.preferences.create(preference);
-        res.json({ id: response.body.id });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error creating preference');
-    }
+app.post("/notify", function (req, res) {
+	console.log('notify');
+	const { body, query } = req;
+	console.log({ body, query });
+	res.send();
 });
 
-app.post('/notify', (req, res) => {
-    try {
-        console.log('notify');
-        const { body, query } = req;
-        console.log({ body, query });
-        res.send();
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Notification error');
-    }
+app.get("/feedback", function (req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	})
 });
 
-app.get('/feedback', (req, res) => {
-    try {
-        res.json({
-            Payment: req.query.payment_id,
-            Status: req.query.status,
-            MerchantOrder: req.query.merchant_order_id
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Feedback error');
-    }
-});
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.use('/uploads', express.static('uploads'));
-
+// errorMiddleware
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+mercadopago.preferences.findById('1180488044-b8242cf5-9aa5-447d-b120-570549edbf15').then(res => console.log(res.body));
+
 app.listen(
-    PORT,
+    PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
 );
