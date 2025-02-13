ac0c4f5de4934f8673d7f5c8b0d85bdaa52ef67c
JUANCHO <juancruzmart93@gmail.com>
2025-02-13 00:24:44 -0300

    a ver el config, mañana pruebo

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,9 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
-process.loadEnvFile()
+dotenv.config()
 
 connectDB()
 
@@ -73,4 +74,4 @@ const PORT = process.env.PORT || 5000
 app.listen(
     PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
-);
+)
